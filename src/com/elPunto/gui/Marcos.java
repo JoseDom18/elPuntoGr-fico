7e4bc4bb41987a71171b44b2f80f48896721e1f2
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-10-03 12:19:17 -0600

    actualizando proyecto

--- src/com/elPunto/gui/Marcos.java
+++ src/com/elPunto/gui/Marcos.java
@@ -1,11 +1,18 @@
 package com.elPunto.gui;
 
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Marcos extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public Marcos() {
 		Toolkit pantalla = Toolkit.getDefaultToolkit();
@@ -15,7 +22,27 @@ public class Marcos extends JFrame{
 		Image icono = pantalla.getImage("src/com/elPunto/gui/elPuntoImg.png");
 		setIconImage(icono);
 
+		Frame frame = new Frame();
+		
+		add(frame);
+
 		setVisible(true);
 	}
 
+}
+
+class Frame extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawString("Primer grafico", 100, 100);
+	}
+	
+	
+	
 }
