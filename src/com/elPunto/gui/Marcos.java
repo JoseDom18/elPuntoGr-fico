96b615c010da50df50b68f7e26ae5a11f084ff70
JoseDom18 <199011605+JoseDom18@users.noreply.github.com>
2025-10-20 15:11:26 -0600

    actualizado

--- src/com/elPunto/gui/Marcos.java
+++ src/com/elPunto/gui/Marcos.java
@@ -1,9 +1,14 @@
 package com.elPunto.gui;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -16,7 +21,11 @@ public class Marcos extends JFrame{
 
 	public Marcos() {
 		Toolkit pantalla = Toolkit.getDefaultToolkit();
-		setExtendedState(MAXIMIZED_BOTH);
+//		setExtendedState(MAXIMIZED_BOTH);
+		Dimension size = pantalla.getScreenSize();
+		int altura = size.height / 2;
+		int ancho = size.width / 2;
+		setBounds(ancho / 2 , altura / 2, ancho, altura);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("elPunto");
 		Image icono = pantalla.getImage("src/com/elPunto/gui/elPuntoImg.png");
@@ -31,17 +40,40 @@ public class Marcos extends JFrame{
 
 }
 
-class Frame extends JPanel {
+class Frame extends JPanel implements ActionListener {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
+	JButton botonAzul = new JButton("Azul");
+	JButton botonAmarillo = new JButton("Amarillo");
+	JButton botonRojo = new JButton("Rojo");
+
+	public Frame() {
+		add(botonAzul);
+		add(botonAmarillo);
+		add(botonRojo);
+		botonAzul.addActionListener(this);
+		botonAmarillo.addActionListener(this);
+		botonRojo.addActionListener(this);
+	}
+
+	public void actionPerformed(ActionEvent	e) {
+		Object botosPulsado = e.getSource();
+		if (botosPulsado == botonAzul) setBackground(Color.BLUE);
+		else if (botosPulsado == botonAmarillo) setBackground(Color.YELLOW);
+		else setBackground(Color.RED);
+
+	}
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.drawString("Primer grafico", 100, 100);
 	}
+
+
 	
 	
 	
